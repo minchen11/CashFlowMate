ef32def969758f9e847915a4d646cb71fcc7d7af
unknown <amienishaq@gmail.com>
2023-11-28 22:12:39 +0800

    first commit

---
+++ app/src/main/java/com/capstone/cashflowmate/ui/Register_Activity.kt
@@ -0,0 +1,12 @@
+package com.capstone.cashflowmate.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.cashflowmate.R
+
+class Register_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
