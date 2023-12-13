db29a910587ce1e21c76086bb9b2ba689e3b87fc
minchen11 <amienishaq@gmail.com>
2023-12-14 06:27:38 +0800

    update fragment home and profile

--- app/src/main/java/com/capstone/cashflowmate/ui/HomeFragment.kt
+++ app/src/main/java/com/capstone/cashflowmate/ui/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.capstone.cashflowmate.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.capstone.cashflowmate.R
 
@@ -14,6 +16,21 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Find the profileHome ImageView by its ID
+        val profileImageView: ImageView = rootView.findViewById(R.id.profileHome)
+
+        // Set a click listener on the profile ImageView
+        profileImageView.setOnClickListener {
+            // Create an Intent to start the ProfileActivity
+            val intent = Intent(requireContext(), ProfileActivity::class.java)
+
+            // Start the ProfileActivity
+            startActivity(intent)
+        }
+
+        return rootView
     }
 }
